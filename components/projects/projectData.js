18a7bc6dda17bf8e698982ecb762326137ca040f
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-31 17:43:42 -1000

    Add project data

---
+++ components/projects/projectData.js
@@ -0,0 +1,25 @@
+const projectData = {
+  {
+    name: 'HALayovers',
+    description: 'Multi-page flight attendant forum for layover tips and experiences',
+    technologies: ['React', 'React Router', 'Express', 'MySQL', 'CSS Modules'],
+    gitHubURL: 'https://github.com/jerfuj/halayovers',
+    liveURL: 'https://halayovers.netlify.app/'
+  },
+  {
+    name: 'DSA Notes',
+    description: 'Next.js application containing my data structures & algorithms notes',
+    technologies: ['Next.js', 'React', 'CSS Modules'],
+    gitHubURL: 'https://github.com/jerfuj/dsa-notes',
+    liveURL: 'https://dsa-notes.vercel.app/'
+  },
+  {
+    name: 'Best Buy Clone',
+    description: 'Main product details microservice for Best Buy’s product page',
+    technologies: ['React', 'Express', 'Mongo DB', 'Mongoose', 'CSS Modules'],
+    gitHubURL: 'https://github.com/hrsjo3-bigbrain/bestbuy-itemdetails',
+    liveURL: 'https://bbproductdetails.netlify.app/'
+  }
+}
+
+export default projectData;
