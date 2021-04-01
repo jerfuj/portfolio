52270a422dc1970d4ac578eebc685af6d9575826
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-31 20:59:00 -1000

    Add image

--- components/projects/projectData.js
+++ components/projects/projectData.js
@@ -1,25 +1,28 @@
-const projectData = {
+const projectData = [
   {
     name: 'HALayovers',
     description: 'Multi-page flight attendant forum for layover tips and experiences',
     technologies: ['React', 'React Router', 'Express', 'MySQL', 'CSS Modules'],
     gitHubURL: 'https://github.com/jerfuj/halayovers',
-    liveURL: 'https://halayovers.netlify.app/'
+    liveURL: 'https://halayovers.netlify.app/',
+    image: '/images/HALayovers-homepage.png'
   },
   {
     name: 'DSA Notes',
     description: 'Next.js application containing my data structures & algorithms notes',
     technologies: ['Next.js', 'React', 'CSS Modules'],
     gitHubURL: 'https://github.com/jerfuj/dsa-notes',
-    liveURL: 'https://dsa-notes.vercel.app/'
+    liveURL: 'https://dsa-notes.vercel.app/',
+    image: '/images/DSA-Notes-homepage.png'
   },
   {
     name: 'Best Buy Clone',
     description: 'Main product details microservice for Best Buy’s product page',
     technologies: ['React', 'Express', 'Mongo DB', 'Mongoose', 'CSS Modules'],
     gitHubURL: 'https://github.com/hrsjo3-bigbrain/bestbuy-itemdetails',
-    liveURL: 'https://bbproductdetails.netlify.app/'
+    liveURL: 'https://bbproductdetails.netlify.app/',
+    image: '/images/item-details-zoomed.png'
   }
-}
+]
 
 export default projectData;
