bdfa899712a7ed71315aee587ab0b7a227917dca
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-31 20:58:02 -1000

    Add projects

--- components/projects/Projects.js
+++ components/projects/Projects.js
@@ -1,12 +1,16 @@
 import styles from './Projects.module.css';
+import projectData from './projectData';
+import ProjectCard from './ProjectCard';
 
 function Projects({ projectsRef }) {
   return (
-    <section 
-      className={styles.projectsContainer}
-      ref={projectsRef}
-    >
+    <section className={styles.projectsContainer} ref={projectsRef}>
       <h1 className={styles.header}>Projects</h1>
+      <ul className={styles.cardsContainer}>
+        {projectData.map((project) => (
+          <ProjectCard project={project} />
+        ))}
+      </ul>
     </section>
   )
 };
