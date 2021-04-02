74137f6fb062a956c7886d8666badd2c61093e42
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-01 20:49:21 -1000

    Add image and overlay

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,10 +1,23 @@
+import Image from 'next/image';
 import styles from './Hero.module.css';
 
 function Hero({ heroRef }) {
   return (
     <section ref={heroRef} className={styles.heroContainer}>
-      <h1 className={styles.header}>Hello! My name is Jeremy</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, odio itaque. Corporis quia, delectus itaque voluptatem provident praesentium incidunt ut, corrupti unde illo soluta facilis aut voluptatibus consequatur, ex neque?</p>
+      <div>
+        <Image
+          alt="Golf course"
+          src="/images/klipper.jpeg"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+      <div className={styles.overlay} />
+      <div className={styles.textContainer}>
+        <h1 className={styles.header}>Jeremy Fujimoto</h1>
+        <p className={styles.p}>Full Stack Web Developer</p>
+      </div>
     </section>
   )
 };
