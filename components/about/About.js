e07b438ff2f47a60d7f56e396bc23620a9e8d7f6
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-01 20:47:20 -1000

    Add about me section

--- components/about/About.js
+++ components/about/About.js
@@ -1,9 +1,19 @@
 import styles from './About.module.css';
+import Technologies from './Technologies';
 
 function About({ aboutRef }) {
   return (
     <section ref={aboutRef} className={styles.aboutContainer}>
-      <h1 className={styles.header}>About</h1>
+      <div className={styles.aboutMeContainer}>
+        <h2>About Me</h2>
+        <p>
+          Hi! My name is Jeremy and I was born and raised in Mililani, Hawaii. I'm a baseball player turned flight attendant turned passionate full stack web developer. I particularly enjoy working on the front end, especially with React, and love the satisfaction that comes with bringing an idea to life.
+        </p>
+        <p>
+          Outside of coding, I enjoy golf and travel!
+        </p>
+      </div>
+      <Technologies />
     </section>
   )
 };
