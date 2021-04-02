58fc4b46297813ce43ffc63e82063fd06bbcc5c7
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-01 20:48:53 -1000

    Change header to "Contact"

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -3,7 +3,7 @@ import styles from './Contact.module.css';
 function Contact({ contactRef }) {
   return (
     <section ref={contactRef} className={styles.contactContainer}>
-      <h1 className={styles.header}>Get in touch!</h1>
+      <h1 className={styles.header}>Contact</h1>
       <form name="contact" method="POST" data-netlify="true" className={styles.form}>
         <div className={styles.formField}>
           <label>Name <input type="text" name="name" /></label>   
