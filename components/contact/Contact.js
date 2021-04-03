665daed95eee9c44edc887c0da2ea16a6553cf68
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-02 22:09:41 -1000

    Add GitHub and Linkedin links

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -1,9 +1,28 @@
+import Image from 'next/image';
 import styles from './Contact.module.css';
 
 function Contact({ contactRef }) {
   return (
     <section ref={contactRef} className={styles.contactContainer}>
-      <h1 className={styles.header}>Contact</h1>
+      <h1 className={styles.header}>Get in touch!</h1>
+      <div className={styles.linksContainer}>
+        <a href="https://www.linkedin.com/in/jeremyfujimoto/" target="blank" className={styles.imgContainer}>
+          <Image
+            alt="linkedin"
+            src="/images/linkedin-logo.png"
+            layout="fill"
+            objectFit="contain"
+          />
+        </a>
+        <a href="https://github.com/jerfuj" target="blank" className={styles.imgContainer}>
+          <Image
+            alt="github"
+            src="/images/github-logo.png"
+            layout="fill"
+            objectFit="contain"
+          />
+        </a>
+      </div>
       <form name="contact" method="POST" data-netlify="true" className={styles.form}>
         <div className={styles.formField}>
           <label>Name <input type="text" name="name" /></label>   
@@ -15,7 +34,9 @@ function Contact({ contactRef }) {
           <label>Message <textarea name="message" rows="8"></textarea></label>
         </div>
         <div className={styles.formField}>
-          <button className={styles.btn} type="submit">Send</button>
+          <button className={styles.btn} type="submit">
+            <span className={styles.btnText}>Send</span>
+          </button>
         </div>
       </form>
     </section>
