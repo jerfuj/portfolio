1a6b5f372644fb2e576941e88eef728a7f30f2dd
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-03 22:51:31 -1000

    Particles component for background

---
+++ components/hero/ParticlesBg.js
@@ -0,0 +1,91 @@
+import Particles from "react-tsparticles";
+
+function ParticlesBg() {
+  return (
+      <Particles
+        width={'100vw'}
+        height={'100vh'}
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#000310",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#22aeff",
+            },
+            // links: {
+            //   color: "",
+            //   distance: 150,
+            //   enable: true,
+            //   opacity: 0.5,
+            //   width: 1,
+            // },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: .5,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 3,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+  )
+};
+
+export default ParticlesBg;
