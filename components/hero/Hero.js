56241666f7fd3f8ab65af564f060e247b4ad1481
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-03 22:51:15 -1000

    Add particles background

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,19 +1,11 @@
 import Image from 'next/image';
 import styles from './Hero.module.css';
+import ParticlesBg from './ParticlesBg';
 
 function Hero({ heroRef }) {
   return (
     <section ref={heroRef} className={styles.heroContainer}>
-      <div>
-        <Image
-          alt="Golf course"
-          src="/images/klipper.jpeg"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-        />
-      </div>
-      <div className={styles.overlay} />
+      <ParticlesBg />
       <div className={styles.textContainer}>
         <h1 className={styles.header}>Jeremy Fujimoto</h1>
         <p className={styles.p}>Full Stack Web Developer</p>
