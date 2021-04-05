94b7e1e13c0f24ff987a6f6a1f3c47b9662d800b
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-04 19:28:04 -1000

    Implement links for hamburger menu

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -2,7 +2,6 @@ import { useState, useEffect, Fragment } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Navbar.module.css';
-import NavLinks from './NavLinks';
 import Hamburger from './Hamburger';
 
 
@@ -35,6 +34,37 @@ export default function Navbar({ projectsRef, aboutRef, contactRef, heroRef }) {
   function BurgerLinks() {
     return (
       <ul className={styles.burgerLinksContainer}>
+        <li onClick={() => {
+          scrollIntoView(projectsRef);
+          toggleBurger();
+        }
+        }>
+          Projects
+        </li>
+        <li onClick={() => {
+          scrollIntoView(aboutRef);
+          toggleBurger();
+        }
+        }>
+          About
+        </li>
+        <li onClick={() => {
+          scrollIntoView(contactRef);
+          toggleBurger();
+        }
+        }>
+          Contact
+        </li>
+        <li className={styles.resume}>
+          <a href="https://docs.google.com/document/d/1TxgDlWCuosxEHGHiO6SpZsRMj8rSBkzHc5Kxqxqr98E/edit#heading=h.au8c9iwzs8ej" target="blank">Resume</a>
+        </li>
+      </ul>
+    )
+  }
+
+  function NavLinks() {
+    return (
+      <ul className={styles.linksContainer}>
         <li onClick={() => scrollIntoView(projectsRef)}>Projects</li>
         <li onClick={() => scrollIntoView(aboutRef)}>About</li>
         <li onClick={() => scrollIntoView(contactRef)}>Contact</li>
@@ -43,7 +73,7 @@ export default function Navbar({ projectsRef, aboutRef, contactRef, heroRef }) {
         </li>
       </ul>
     )
-  }
+  };
 
   return (
     <>
@@ -56,7 +86,7 @@ export default function Navbar({ projectsRef, aboutRef, contactRef, heroRef }) {
             <Image src="/images/JF-logo.png" height={55} width={55} />
           </div>
           <Hamburger toggleBurger={toggleBurger} />
-          {/* <NavLinks /> */}
+          <NavLinks />
         </div>
         {menuOpen && <BurgerLinks />}
       </header>
