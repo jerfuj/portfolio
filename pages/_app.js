b799a9061713344540f854b69a84eb83db28636b
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-04 19:56:19 -1000

    Add title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import { Fragment } from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Jeremy Fujimoto</title>
+        <link rel="shortcut icon" href="/images/JF-logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
