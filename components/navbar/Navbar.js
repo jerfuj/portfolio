f27139c4cb9074d061e60d2417e4947cb44ce2a1
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-04 22:59:54 -1000

    Implement 2 className changes on scroll

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, Fragment } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from './Navbar.module.css';
 import Hamburger from './Hamburger';
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 
 export default function Navbar({ projectsRef, aboutRef, contactRef, heroRef }) {
   const [navClass, setNavClass] = useState(`${styles.navContent}`);
+  const [mainNavClass, setMainNavClass] = useState(`${styles.nav}`)
 
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleBurger = () => {
@@ -13,14 +14,16 @@ export default function Navbar({ projectsRef, aboutRef, contactRef, heroRef }) {
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', changeBackground);
+    window.addEventListener('scroll', scrollDown);
   }, [])
   
-  const changeBackground = () => {
+  const scrollDown = () => {
     if (window.scrollY >= 80) {
       setNavClass(`${styles.navContent} ${styles.navScrollDown}`)
+      setMainNavClass(`${styles.nav} ${styles.changeBg}`)
     } else {
       setNavClass(`${styles.navContent}`);
+      setMainNavClass(`${styles.nav}`);
     }
   }
 
@@ -79,22 +82,20 @@ export default function Navbar({ projectsRef, aboutRef, contactRef, heroRef }) {
   }
 
   return (
-    <>
-      <header className={styles.nav}>
-        <div className={navClass}>
-          <div 
-            className={styles.logoContainer}
-            onClick={() => scrollIntoView(heroRef)}
-          >
-            <Image src="/images/JF-logo.png" height={55} width={55} />
-          </div>
-          <Hamburger toggleBurger={toggleBurger} />
-          <NavLinks />
+    <header className={mainNavClass}>
+      <div className={navClass}>
+        <div 
+          className={styles.logoContainer}
+          onClick={() => scrollIntoView(heroRef)}
+        >
+          <Image src="/images/JF-logo.png" height={55} width={55} />
         </div>
-        {menuOpen && <BurgerLinks />}
-      </header>
-    </>
-  )
+        <Hamburger toggleBurger={toggleBurger} />
+        <NavLinks />
+      </div>
+      {menuOpen && <BurgerLinks />}
+    </header>
+)
 }
 
 Navbar.propTypes = {
