44a87bb5637ff164cff37fe0ac1fe271778e724b
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-04-06 10:35:52 -1000

    Fix canvas taking up the entire page

--- components/hero/ParticlesBg.js
+++ components/hero/ParticlesBg.js
@@ -1,81 +1,84 @@
 import Particles from "react-tsparticles";
+import styles from './Hero.module.css';
 
 function ParticlesBg() {
   return (
-      <Particles
-        id="tsparticles"
-        options={{
-          background: {
-            color: {
-              value: "#000310",
+      <div className={styles.frame}>
+        <Particles
+          id="tsparticles"
+          options={{
+            background: {
+              color: {
+                value: "#000310",
+              },
             },
-          },
-          fpsLimit: 60,
-          interactivity: {
-            detectsOn: "canvas",
-            events: {
-              onClick: {
+            fpsLimit: 60,
+            interactivity: {
+              detectsOn: "canvas",
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#22aeff",
+              },
+              collisions: {
                 enable: true,
-                mode: "push",
               },
-              onHover: {
+              move: {
+                direction: "none",
                 enable: true,
-                mode: "repulse",
+                outMode: "bounce",
+                random: false,
+                speed: .5,
+                straight: false,
               },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
+              number: {
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+                value: 80,
               },
-              push: {
-                quantity: 4,
+              opacity: {
+                value: 0.5,
               },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 3,
               },
             },
-          },
-          particles: {
-            color: {
-              value: "#22aeff",
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: .5,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              random: true,
-              value: 3,
-            },
-          },
-          detectRetina: true,
-        }}
-      />
+            detectRetina: true,
+          }}
+        />
+      </div>
   )
 }
 
